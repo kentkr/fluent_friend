db1d4c6b8e55acb8d060fc89e0b2544ce759d168
Kyle Kent <kyle.kent321@gmail.com>
2023-06-04 11:54:41 -0700

    added corrections to separated user message

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,18 +9,18 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         # convert json to string
         message = json.loads(text_data)['text']
+        # get correction - dont do aything with it yet
+        gpt_correction = await get_gpt_correction(message)
         # add to user message
-        user_dict = {'text':{'msg':message, 'source':'user'}}
+        user_dict = {'text':{'msg':message, 'source':'user', 'correction':gpt_correction}}
         # immediately send to frontend
         await self.send(text_data=json.dumps(user_dict))
 
         # get chat gpt response
         gpt_r = await get_gpt_response(message)
         # bot dict
-        bot_dict = {'text':{'msg':gpt_r, 'source':'bot'}}
+        bot_dict = {'text':{'msg':gpt_r, 'source':'bot', 'correction': None}}
         await self.send(text_data=json.dumps(bot_dict))
 
-        # get correction - dont do aything with it yet
-        gpt_correction = await get_gpt_correction(message)
         print(gpt_correction)
 
