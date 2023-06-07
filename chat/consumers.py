2231094ee36a1951ff3e8065f0a03776f1f3e21a
Kyle Kent <kyle.kent321@gmail.com>
2023-06-06 17:40:57 -0700

    actually made async consumer async

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,23 +4,38 @@ from asgiref.sync import async_to_sync
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .tasks import get_gpt_response, get_gpt_correction
+import asyncio
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         # convert json to string
         message = json.loads(text_data)['text']
+        
+        asyncio.create_task(self.send_user_message(message))
+        asyncio.create_task(self.send_gpt_message(message))
+
+
+
         # get correction - dont do aything with it yet
-        gpt_correction = await get_gpt_correction(message)
+        #gpt_correction = await get_gpt_correction(message)
         # add to user message
-        user_dict = {'text':{'msg':message, 'source':'user', 'correction':gpt_correction}}
-        # immediately send to frontend
-        await self.send(text_data=json.dumps(user_dict))
 
-        # get chat gpt response
-        gpt_r = await get_gpt_response(message)
-        # bot dict
-        bot_dict = {'text':{'msg':gpt_r, 'source':'bot', 'correction': None}}
-        await self.send(text_data=json.dumps(bot_dict))
+    async def task1(self, v):
+        await asyncio.sleep(5)
+        print('--- task 1 ----')
 
-        print(gpt_correction)
+    async def task2(self, v):
+        print('--- task 2 ----')
+
+    async def send_user_message(self, message):
+        # add to user message
+        user_dict = json.dumps({'text':{'msg':message, 'source':'user', 'correction':'place_holder'}})
+        await self.send(text_data=user_dict)
+
+    async def send_gpt_message(self, message):
+        response = await get_gpt_response(message)
+        bot_dict = json.dumps({'text':{'msg':response, 'source':'bot', 'correction': None}})
+        print(bot_dict)
+        await asyncio.sleep(2)
+        await self.send(text_data=bot_dict)
 
