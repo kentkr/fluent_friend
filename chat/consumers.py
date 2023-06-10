2679c7db7ab3d1a9d017e7c1cd2e30d53453bc0e
Kyle Kent <kyle.kent321@gmail.com>
2023-06-10 16:04:37 -0700

    created message ids

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,34 +8,23 @@ import asyncio
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
+        print(json.loads(text_data))
         # convert json to string
-        message = json.loads(text_data)['text']
+        data = json.loads(text_data)
         
-        asyncio.create_task(self.send_user_message(message))
-        asyncio.create_task(self.send_gpt_message(message))
+        asyncio.create_task(self.send_user_message(data['message'], data['message_id']))
+        asyncio.create_task(self.send_gpt_message(data['message'], data['message_id']))
 
-
-
-        # get correction - dont do aything with it yet
-        #gpt_correction = await get_gpt_correction(message)
+    async def send_user_message(self, message, message_id):
         # add to user message
-
-    async def task1(self, v):
-        await asyncio.sleep(5)
-        print('--- task 1 ----')
-
-    async def task2(self, v):
-        print('--- task 2 ----')
-
-    async def send_user_message(self, message):
-        # add to user message
-        user_dict = json.dumps({'text':{'msg':message, 'source':'user', 'correction':'place_holder'}})
+        user_dict = json.dumps({'text':{'msg':message, 'source':'user', 'correction':None}})
         await self.send(text_data=user_dict)
 
-    async def send_gpt_message(self, message):
+    async def send_gpt_message(self, message, message_id):
         response = await get_gpt_response(message)
-        bot_dict = json.dumps({'text':{'msg':response, 'source':'bot', 'correction': None}})
-        print(bot_dict)
+        bot_dict = json.dumps({'text':{'msg':response + '1', 'source':'bot', 'correction': None}})
+        await self.send(text_data=bot_dict)
         await asyncio.sleep(2)
+        bot_dict = json.dumps({'text':{'msg':response + '2', 'source':'bot', 'correction': None}})
         await self.send(text_data=bot_dict)
 
