e6dbe3839f43874a6b86e93edc098246ffb8626b
Kyle Kent <kyle.kent321@gmail.com>
2023-07-06 16:57:38 -0700

    keeping a message history log

--- chat/consumers.py
+++ chat/consumers.py
@@ -26,6 +26,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         await self.send(text_data=user_dict)
 
     async def send_gpt_message(self, message_id, message_history):
+        print(message_history)
         # get last six messages
         response = await get_gpt_response(message_history[-6:])
         # add to message history
