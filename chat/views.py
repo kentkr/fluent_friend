3857413206926d042afb3fa18fe19ce26202d55a
Kyle Kent <kyle.kent321@gmail.com>
2025-01-23 20:12:51 -0600

    some journal entry progress

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,7 @@
 from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
+from .models import JournalEntry
 
 
 class ChatView(TemplateView):
@@ -7,5 +10,13 @@ class ChatView(TemplateView):
 class AboutView(TemplateView):
     template_name: str = "chat/about.html"
 
+@method_decorator(login_required, name='dispatch')
 class JournalEntryView(TemplateView):
     template_name: str = "chat/journal_entry.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user_id = self.request.user.id
+        entries = JournalEntry.objects.filter(user_id=user_id)
+        context['entries'] = entries
+        return context
