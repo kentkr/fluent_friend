fa424f0058bea354a7f9f6c9485b175a73ce966b
Kyle Kent <kyle.kent321@gmail.com>
2025-02-21 21:16:41 -0600

    bruh im so api pilled

--- chat/urls.py
+++ chat/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ChatView, AboutView, JournalEntryView
+from .views import ChatView, AboutView, JournalEntryView, get_journal_entry, get_journal_titles
 
 
 app_name = "chat"
@@ -9,4 +9,6 @@ urlpatterns = [
     path("about", AboutView.as_view(), name="about_view"),
     path("about", AboutView.as_view(), name="about_view"),
     path("journal_entry", JournalEntryView.as_view(), name="journal_entry_view"),
+    path("api/get_journal_entry/", get_journal_entry, name="get_journal_entry"),
+    path("api/get_journal_titles/", get_journal_titles, name="get_journal_titles"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpRequest, JsonResponse
+from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
 from .models import JournalEntry
 
 
@@ -22,12 +24,27 @@ class JournalEntryView(TemplateView):
         context['entries'] = entries
         return context
 
-    def post(self, request, *args, **kwargs):
-        entry_id = request.POST.get('entry_id')
-        if entry_id:
-            entry = get_object_or_404(JournalEntry, id=entry_id, user_id=request.user.id)
-            context = self.get_context_data()
-            context['selected_entry'] = entry
-            return render(request, self.template_name, context)
 
-        return self.get(request, *args, **kwargs)
+@require_http_methods(['GET'])
+@login_required
+def get_journal_titles(request: HttpRequest) -> JsonResponse:
+    user_id = request.user.id
+    entries = JournalEntry.objects.filter(user_id = user_id).values('id', 'entry_name')
+    response = JsonResponse({
+        'tmp': list(entries),
+    })
+    return response
+    #return JsonResponse({'error': 'entry id not provided'})
+
+@require_http_methods(['GET'])
+@login_required
+def get_journal_entry(request: HttpRequest) -> JsonResponse:
+    entry_id = request.GET.get('entry_id')
+    if entry_id:
+        entry = get_object_or_404(JournalEntry, id=entry_id, user_id=request.user.id)
+        response = JsonResponse({
+            'entry_name': entry.entry_name,
+            'text': entry.text
+        })
+        return response
+    return JsonResponse({'error': 'entry id not provided'})
