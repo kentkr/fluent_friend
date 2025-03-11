4fd1a10d148f4b42810860c1d9a992e930ebf40d
Kyle Kent <kyle.kent321@gmail.com>
2025-03-10 21:33:43 -0500

    sheeeesh (can delete/create journal entires, still needs some work)

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
     path("journal_entries/", views.JournalListCreate.as_view(), name="journal-entries"),
-    path("journal_entries/create/", views.JournalEntryInit.as_view(), name="journal-entries-init"),
+    path("journal_entries/delete/<int:pk>/", views.JournalEntryDelete.as_view(), name="delete-journal-entry"),
 ]
