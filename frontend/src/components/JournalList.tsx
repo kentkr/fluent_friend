e8efa37f2b406a6491bb1127c7910f4b4896d683
Kyle Kent <kyle.kent321@gmail.com>
2025-03-10 19:09:38 -0500

    ok ok first real component

--- frontend/src/components/JournalList.tsx
+++ frontend/src/components/JournalList.tsx
@@ -1,31 +1,29 @@
 import '../styles/theme.css'
 
-function JournalList(): JSX.Element {
+interface JournalListProps {
+    entries: Array<Entry>;
+}
+
+function onDelete() {
+    console.log('delete');
+}
+
+function onEdit() {
+    console.log('edit');
+}
+
+function JournalList({ entries }: JournalListProps): JSX.Element {
     return ( 
         <>
             <div className="p-2 w-1/4 border boder-background bg-foreground flex flex-col">
                 <h1>Journal entries</h1>
                 <div className='border border-background p-2 flex-1'>
                     <ol>
-                        <li className='flex justify-between m-1'>
-                            <p>
-                                first
-                            </p>
-                            <div>
-                            <button>e</button>
-                            <button>-</button>
-                            </div>
-                        </li>
-                        <hr/>
-                        <li className='flex justify-between m-1'>
-                            <p>
-                                second
-                            </p>
-                            <div>
-                            <button>e</button>
-                            <button>-</button>
-                            </div>
-                        </li>
+                    {
+                        entries.map((entry) => (
+                            <Tmp entry={entry} onDelete={onDelete} onEdit={onEdit} key={entry.id}/>
+                        ))
+                    }
                     </ol>
                     <button>+</button>
                 </div>
@@ -34,5 +32,31 @@ function JournalList(): JSX.Element {
     )
 }
 
+interface Entry {
+    id: number;
+    user: number;
+    title: string;
+    text: string
+} 
+
+interface TmpProps {
+    entry: Entry;
+    onDelete: CallableFunction;
+    onEdit: CallableFunction;
+}
+
+function Tmp({ entry, onDelete, onEdit }: TmpProps): JSX.Element {
+    return (
+        <>
+            <li className='flex justify-between m-1' id={entry.id.toString()}>
+                <p>{entry.title}</p>
+                <div>
+                <button onClick={() => onDelete(entry.id)}>e</button>
+                <button onClick={() => onEdit(entry.id)}>-</button>
+                </div>
+            </li>
+        </>
+    )
+}
 
 export default JournalList
