1812c42fe44e28d7132de6be9efe2990fb9bcdad
Kyle Kent <kyle.kent321@gmail.com>
2025-03-14 17:07:48 -0500

    almost got the text update flow to work

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
     path("journal_entries/", views.JournalListCreate.as_view(), name="journal-entries"),
     path("journal_entries/delete/<int:pk>/", views.JournalEntryDelete.as_view(), name="delete-journal-entry"),
+    path("journal_entries/update/<int:id>/", views.JournalEntryUpdate.as_view(), name="update-journal-entry"),
 ]
--- frontend/src/pages/Journal.tsx
+++ frontend/src/pages/Journal.tsx
@@ -5,6 +5,7 @@ import { EditorContent, useEditor } from '@tiptap/react'
 import Underline from '@tiptap/extension-underline'
 import JournalList from '../components/JournalList'
 import '../styles/Journal.css'
+import { Dispatch, SetStateAction, useRef } from 'react'
 
 // api
 import api from "../api";
@@ -23,9 +24,36 @@ interface Entries {
 
 function Journal() {
     const [entries, setEntries] = useState<EntryObj[]>([]);
+    const [currEntry, setCurrEntry] = useState<EntryObj>({id: -1, user: -1, title: null, text: null});
+
     useEffect(() => {
         getEntries();
     }, []);
+
+    // update title or text of current entry if it changes
+    const prevTitle = useRef(currEntry.title);
+    useEffect(() => {
+        if (currEntry.title !== prevTitle.current) {
+            setEntries(prevEntries => {
+                const updatedEntries = [...prevEntries]
+                for (let i = 0; i < updatedEntries.length; i++) {
+                    let entry = updatedEntries[i]
+                    if (entry.id == currEntry.id) {
+                        updatedEntries[i] = { ...updatedEntries[i], title: currEntry.title  }
+                    }
+                }
+                return updatedEntries;
+            });
+        }
+        // update entry
+        if (currEntry.id !== -1) {
+            console.log(`updating ${currEntry.id}, ${currEntry.title} ${currEntry.text}`)
+            api
+                .put(`/api/journal_entries/update/${currEntry.id}/`, currEntry)
+                .catch((err) => alert(err));
+        }
+
+    }, [currEntry])
 
     const getEntries = () => {
         api
@@ -33,11 +61,12 @@ function Journal() {
             .then((res) => res.data)
             .then((data) => {
                 setEntries(data);
+                setCurrEntry(data[0])
                 console.log(data);
             })
             .catch((err) => alert(err));
     };
-    window.tmp = entries;
+    window.tmp = currEntry;
 
     function newEntry() {
         console.log('adding new entry')
@@ -45,7 +74,9 @@ function Journal() {
             .post("/api/journal_entries/", {})
             .then((res) => res.data)
             .then((data) => {
-                setEntries(entries => [...entries, data])
+                setEntries(entries => [...entries, data]);
+                console.log(data)
+                setCurrEntry(data);
             })
     }
 
@@ -58,34 +89,102 @@ function Journal() {
             .then((data) => {
                 setEntries(prevEntries => prevEntries.filter(entry => entry.id !== id))
             })
-
     }
 
   return (
       <>
         <div className='page-container'>
-            <JournalList entries={entries} newEntry={newEntry} deleteEntry={deleteEntry}/>
-            <Editor/>
+            <JournalList entries={entries} newEntry={newEntry} deleteEntry={deleteEntry} setCurrEntry={setCurrEntry}/>
+            <Editor currEntry={currEntry} setCurrEntry={setCurrEntry}/>
         </div>
       </>
   )
 }
 
 
-declare module '@tiptap/core' {
-  interface Commands<ReturnType> {
-    suggestion: {
-      /**
-       * Set the suggestion mark
-       */
-      setSuggestion: () => ReturnType,
-      /**
-       * Unset the suggestion mark
-       */
-      unsetSuggestion: () => ReturnType
+function Editor({ currEntry, setCurrEntry } : { currEntry: EntryObj; setCurrEntry: Dispatch<SetStateAction<EntryObj>> }) {
+    const prevEditorContent = useRef(currEntry.text);
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Underline,
+            SuggestionMark
+        ],
+        editorProps: {
+            attributes: {
+                class: 'editor'
+            }
+        },
+        content: `<p>${currEntry.text}</p>`,
+        onUpdate({ editor, transaction }) {
+            setCurrEntry(prevCurrEntry => ({
+                ...prevCurrEntry, text: editor.getText()
+            }))
+            prevEditorContent.current = editor.getText()
+        },
+    })
+
+
+    if (!editor) {
+        return null;
     }
+
+    // on currentry change then set content of editor
+    useEffect(() => {
+        if (prevEditorContent.current != currEntry.text) {
+            editor.commands.setContent(currEntry.text)
+            prevEditorContent.current = currEntry.text
+        }
+    }, [currEntry])
+
+    window.editor = editor;
+
+    return (
+        <div className='editor-container-0'>
+            <Title currEntry={currEntry} setCurrEntry={setCurrEntry}/>
+            <hr></hr>
+            <EditorContent editor={editor} className='editor-container-1'/>
+        </div>
+    )
+}
+
+function Title({ currEntry, setCurrEntry } : { currEntry: EntryObj; setCurrEntry: Dispatch<SetStateAction<EntryObj>> }) {
+    const prevEditorContent = useRef(currEntry.title);
+    const title = useEditor({
+        extensions: [
+            StarterKit,
+        ],
+        content: currEntry.title,
+        onUpdate({ editor, transaction }) {
+            setCurrEntry(prevCurrEntry => ({
+                ...prevCurrEntry, title: editor.getText()
+            }))
+            prevEditorContent.current = editor.getText();
+        },
+    })
+
+    if (!title) {
+        return null
+    }
+
+    useEffect(() => {
+        if (prevEditorContent.current !== currEntry.title) {
+            title.commands.setContent(currEntry.title);
+            prevEditorContent.current = currEntry.title;
+        }
+    }, [currEntry])
+    return <EditorContent editor={title}/>
+}
+
+export default Journal
+
+declare global {
+  interface Window {
+    editor: any; // Replace 'any' with a more specific type if possible
+    tmp: any;
   }
 }
+
 
 const SuggestionMark = Mark.create({
     name: 'suggestion',
@@ -120,47 +219,17 @@ const SuggestionMark = Mark.create({
 })
 
 
-declare global {
-  interface Window {
-    editor: any; // Replace 'any' with a more specific type if possible
-    tmp: any;
+declare module '@tiptap/core' {
+  interface Commands<ReturnType> {
+    suggestion: {
+      /**
+       * Set the suggestion mark
+       */
+      setSuggestion: () => ReturnType,
+      /**
+       * Unset the suggestion mark
+       */
+      unsetSuggestion: () => ReturnType
+    }
   }
 }
-
-
-function Editor() {
-    const editor = useEditor({
-        extensions: [
-            StarterKit,
-            Underline,
-            SuggestionMark
-        ],
-        editorProps: {
-            attributes: {
-                class: 'editor'
-            }
-        },
-        content: '<p>underline this text</p>',
-        onUpdate({ editor, transaction }) {
-            // track text/node/mark changes 
-            console.log('Editor updated');
-            console.log(transaction)
-        },
-    })
-
-    window.editor = editor;
-
-    if (!editor) {
-        return null;
-    }
-
-    return (
-        <div className='editor-container-0'>
-            <h2 className='title'>Title</h2>
-            <hr></hr>
-            <EditorContent editor={editor} className='editor-container-1'/>
-        </div>
-    )
-}
-
-export default Journal
