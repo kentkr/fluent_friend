f48e371098a1593f9b39e51575bce749543e75d5
Kyle Kent <kyle.kent321@gmail.com>
2025-05-11 13:53:02 -0500

    tooltip messy but set up

--- frontend/src/features/journal/components/tooltip/Tooltip.tsx
+++ frontend/src/features/journal/components/tooltip/Tooltip.tsx
@@ -1,10 +1,44 @@
+import { Replacement } from "../../api/lt.d"
+import { ToolTipInfo } from "../../pm/suggestion.d"
+import BubbleMenu from "../bubblemenu/BubbleMenu"
+import { Editor  } from '@tiptap/react'
 
-function Tooltip () {
-  return <>
-    <div>
-      hi
-    </div>
-  </>
+function Message({ text, className }: { text: string, className: string }) {
+  if (!text) return
+  return (
+    <p className={className} >{text}</p>
+  )
+}
+
+function Replacements({ className, list }: { className: string, list: any[] }) {
+  if (!list || list.length === 0) return
+  return (
+    <ol className='flex'>
+      {list.map((replacement: Replacement) => 
+        <li className='p-1' key={replacement.value}>
+          <button className='bg-green'>{replacement.value.replace(/^ | $|^$/g, '·')} </button>
+        </li>
+      )}
+    </ol>
+  )
+}
+
+function Tooltip ({ editor, tti }: { editor: Editor, tti: ToolTipInfo | undefined }) {
+  if (!editor || !tti) return
+
+  const shortMessage = tti.suggSpec.ltMatch.shortMessage
+  const message = tti.suggSpec.ltMatch.message
+  const replacements = tti.suggSpec.ltMatch.replacements
+
+  return (
+    <BubbleMenu editor={editor} open={tti.open}>
+      <div className='bg-foreground border p-1'>
+        <Message className='p-1' text={shortMessage}/>
+        <Message className='p-1' text={message}/>
+        <Replacements className='p-1' list={replacements} />
+      </div>
+    </BubbleMenu>
+  )
 }
 
 export default Tooltip
