b89fe014fa29af19a8458515fb881caadfa4d77b
Kyle Kent <kyle.kent321@gmail.com>
2025-05-11 17:47:14 -0500

    stashing tooltip updates - can now replace text with buttons

--- frontend/src/features/journal/components/tooltip/Tooltip.tsx
+++ frontend/src/features/journal/components/tooltip/Tooltip.tsx
@@ -2,6 +2,8 @@ import { Replacement } from "../../api/lt.d"
 import { ToolTipInfo } from "../../pm/suggestion.d"
 import BubbleMenu from "../bubblemenu/BubbleMenu"
 import { Editor  } from '@tiptap/react'
+import { Range } from '@tiptap/core'
+import './Tooltip.css'
 
 function Message({ text, className }: { text: string, className: string }) {
   if (!text) return
@@ -10,17 +12,23 @@ function Message({ text, className }: { text: string, className: string }) {
   )
 }
 
-function Replacements({ list }: { list: any[] }) {
+function Replacements({ list, range, replaceText }: { list: any[], range: Range, replaceText: CallableFunction }) {
   if (!list || list.length === 0) return
+
   return (
-    <ol className='flex'>
+    <ol className='flex flex-wrap'>
       {list.map((replacement: Replacement) => 
         <li className='p-1' key={replacement.value}>
-          <button className='bg-green'>
+          <button className='replacement' onClick={() => replaceText(range, replacement.value)}>
             {replacement.value.replace(/^ | $|^$/g, '·')} 
           </button>
         </li>
       )}
+      <li>
+        <button className="ignore">
+          Ignore
+        </button>
+      </li>
     </ol>
   )
 }
@@ -31,16 +39,33 @@ function Tooltip ({ editor, tti }: { editor: Editor, tti: ToolTipInfo | undefine
   const shortMessage = tti.suggSpec.ltMatch.shortMessage
   const message = tti.suggSpec.ltMatch.message
   const replacements = tti.suggSpec.ltMatch.replacements
+  //const range = {from: tti.suggSpec.ltMatch.offset, to: tti.suggSpec.ltMatch.offset + tti.suggSpec.ltMatch.length}
   
-  console.log(tti.suggSpec.ltMatch)
-  console.log(tti.suggSpec.ltMatch.rule.issueType, tti.suggSpec.ltMatch.type)
+  // TODO: we can probably put this in Replacements, just pass editor to it
+  const range = {
+    from: tti.suggSpec.ltMatch.offset+1, 
+    to: tti.suggSpec.ltMatch.offset + tti.suggSpec.ltMatch.length+1
+  };
+  
+  function replaceText(range: { from: number, to: number }, replacement: string) {
+    try {
+      // Make sure both range and replacement are properly defined
+      if (range && typeof range.from === 'number' && typeof range.to === 'number' && replacement) {
+        editor.commands.insertContentAt(range, replacement);
+      } else {
+        console.error("Invalid range or replacement", { range, replacement });
+      }
+    } catch (error) {
+      console.error("Error replacing text:", error);
+    }
+  }
 
   return (
     <BubbleMenu editor={editor} open={tti.open}>
-      <div className='bg-foreground border p-1'>
+      <div className='tooltip'>
         <Message className='p-1' text={shortMessage}/>
         <Message className='p-1' text={message}/>
-        <Replacements list={replacements} />
+        <Replacements list={replacements} range={range} replaceText={replaceText}/>
       </div>
     </BubbleMenu>
   )
