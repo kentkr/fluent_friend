3ff29cbe92dacc32bc88f2ccf652f69710afcbf0
Kyle Kent <kyle.kent321@gmail.com>
2025-06-07 16:09:31 -0500

    added loader animation

--- frontend/src/app/pages/chat/Chat.tsx
+++ frontend/src/app/pages/chat/Chat.tsx
@@ -2,15 +2,44 @@ import { useState, useReducer, useRef, useEffect } from 'react'
 import Input from '../../../features/chat/components/input/Input'
 import Message from '../../../features/chat/components/message/Message'
 import './Chat.css'
+import '/Users/kylekent/Desktop/fluent_friend/frontend/src/features/chat/components/message/Message.css'
+import DotLoader from '../../../components/dotloader/DotLoader'
 
-interface MessageProps {
+interface MessageObj {
   id: number;
   message: string;
   sender: string;
+  correction?: string;
+}
+
+function AiMessage({ message }: { message: MessageObj }) {
+  return (
+    <div className='ai-message'>
+      {
+        message.message === undefined ?
+          <DotLoader color='var(--color-foreground)' /> :
+          <p>{message.message}</p> 
+      }
+    </div>
+  )
+}
+
+function UserMessage({ message }: { message: MessageObj }) {
+  return (
+    <div className='user-message'>
+      <p>{message.message}</p>
+      <hr></hr>
+      {
+        message.correction === undefined ? 
+          <DotLoader color='var(--color-background)' /> :
+          <p>message.correction</p>
+      }
+    </div>
+  )
 }
 
 function Chat() {
-  const [messages, setMessages] = useState<MessageProps[]>([
+  const [messages, setMessages] = useState<MessageObj[]>([
     {id: Date.now(), message: 'first', sender: 'ai'}
   ])
   const ws = useRef(null as unknown as WebSocket)
@@ -45,16 +74,16 @@ function Chat() {
     })
   }
 
+  window.messages = messages
   return <>
     <div className='chat-page'>
       <div className='message-container'>
         <ol id='message-list'>
           {messages.map(msg => (
-            <Message 
-              key={msg.id} 
-              message={msg.message} 
-              sender={msg.sender} 
-            />
+            <li key={msg.id}>
+              {msg.sender === 'ai' && <AiMessage message={msg} />}
+              {msg.sender === 'user' && <UserMessage message={msg} />}
+            </li>
           ))}
         </ol>
       </div>
