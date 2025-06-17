09263e26f2dad537a56460d6071e055874f14367
Kyle Kent <kyle.kent321@gmail.com>
2025-06-17 09:10:15 -0500

    - new entry puts entry at the top - clear journal page on delete - fixed journal bug on new user

--- frontend/src/constants.ts
+++ frontend/src/constants.ts
@@ -1,3 +1,4 @@
 export const ACCESS_TOKEN = "access";
 export const REFRESH_TOKEN = "refresh"
-export const JOURNAL_TITLE_PLACEHOLDER = 'Log 10, still no pussy...'
+export const JOURNAL_TITLE_PLACEHOLDER = 'Another long day ...'
+export const JOURNAL_EDITOR_PLACEHOLDER = 'When will it end ...'
--- frontend/src/features/journal/components/entry/Entry.tsx
+++ frontend/src/features/journal/components/entry/Entry.tsx
@@ -6,12 +6,15 @@ import { Tooltip } from "@mui/material";
 import { JOURNAL_TITLE_PLACEHOLDER } from "../../../../constants";
 
 export function Entry({ entry, deleteEntry, selectEntry }: EntryProps): JSX.Element {
-  const titleOrPlaceholder = entry.title === '' ? JOURNAL_TITLE_PLACEHOLDER : entry.title
+  const titleEmpty = entry.title === null || entry.title === ''
+  const titleOrPlaceholder = titleEmpty ? JOURNAL_TITLE_PLACEHOLDER : entry.title
   return (
     <>
       <li className='entry-li' id={entry.id.toString()}>
         <Tooltip title={titleOrPlaceholder} placement="top">
-          <p className={entry.title === '' ? 'entry-title use-placeholder' : 'entry-title'}>{titleOrPlaceholder}</p>
+          <p className={titleEmpty ? 'entry-title use-placeholder' : 'entry-title'}>
+            {titleOrPlaceholder}
+          </p>
         </Tooltip>
         <div className="button-div">
           <button 
