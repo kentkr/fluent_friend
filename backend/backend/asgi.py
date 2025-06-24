90246d6446ec705debd3a52924e3088dbb7ae61a
Kyle Kent <kyle.kent321@gmail.com>
2025-06-24 13:46:01 -0500

    docker compose fully works - time for better env handling

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -9,18 +9,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
+# Initialize Django ASGI application early to ensure the AppRegistry
+# is populated before importing code that may import ORM models.
+from django.core.asgi import get_asgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
-from django.core.asgi import get_asgi_application
 from api.urls import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'backend.settings')
-
-# Initialize Django ASGI application early to ensure the AppRegistry
-# is populated before importing code that may import ORM models.
-django_asgi_app = get_asgi_application()
-
 
 application = ProtocolTypeRouter(
     {
