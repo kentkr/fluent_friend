4eef9cdf27c847d89794f05ca4c5bb297ae65dac
Kyle Kent <kyle.kent321@gmail.com>
2025-06-24 16:04:20 -0500

    minor but incomplete docker changes - envs arent complete

--- frontend/vite-env.d.ts
+++ frontend/vite-env.d.ts
@@ -2,7 +2,7 @@ interface ImportMetaEnv {
   readonly VITE_API_URL: string;
   readonly VITE_LT_URL: string;
   readonly VITE_WS_URL: string;
-  readonly ALLOWED_HOSTS: string;
+  readonly VITE_ALLOWED_HOSTS: string;
 }
 
 interface ImportMeta {
