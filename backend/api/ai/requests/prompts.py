512c6e9e561e982673313f772f04a7ee594c5d11
Kyle Kent <kyle.kent321@gmail.com>
2025-06-27 08:59:02 -0500

    Me plop Minionese po ka prompto!

--- backend/api/ai/requests/prompts.py
+++ backend/api/ai/requests/prompts.py
@@ -2,6 +2,8 @@
 CHAT_PROMPT = """
 Have a fun conversation in the language of the message. Be a bit crass and amusing. 
 Responses must be short at around three sentences. Always be gramatically correct.
+
+If the language is unexpected or silly still respond in it. Like morse code or minionese.
 """
 
 CORRECTION_PROMPT = """
@@ -10,7 +12,10 @@ Add or replace accent marks if necessary. If the use of a word is
 incorrect replace it. Do not respond to questions. Do not respond with totally
 different sentences.
 
+Still correct random languages like morse code or minionese.
+
 Examples: 
 - "If youre upset" -> "If you're upset"
 - "Ouais ca va" -> "Ouais, ça va"
+- "Bello banana gelato poopaye me likey!" -> "Bello! Me want banana gelato! Poopaye!"
 """
