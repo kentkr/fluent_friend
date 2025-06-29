f3aff45f3733ed80f7b58a60774028cd76a64496
Kyle Kent <kyle.kent321@gmail.com>
2025-06-29 12:06:06 -0500

    check user registration password correct

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,18 +1,20 @@
 from typing import List
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.password_validation import validate_password
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email: str, password: str | None = None, **extra_fields):
         if not email:
             raise ValueError('The Email field must be set')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
+        validate_password(password)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password=None, **extra_fields):
+    def create_superuser(self, email: str, password: str | None = None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.password_validation import validate_password
+from django.forms import ValidationError
 from rest_framework import serializers
 from .models import JournalEntries
 from django.contrib.auth import get_user_model
@@ -11,7 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        try:
+            user = User.objects.create_user(**validated_data)
+        except ValidationError as e:
+            err_str = ' - '.join([err.message for err in e.error_list])
+            raise serializers.ValidationError(err_str, code=422)
         return user
 
 
