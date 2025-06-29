277b42f5a7e08778a1e52d3a4db91919e8bde264
Kyle Kent <kyle.kent321@gmail.com>
2025-06-29 11:13:06 -0500

    custom user with email address validation

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,16 +1,16 @@
-from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import JournalEntries
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = ["id", "email", "password"]
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        print(validated_data)
         user = User.objects.create_user(**validated_data)
         return user
 
